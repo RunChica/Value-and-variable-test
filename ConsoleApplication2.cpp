507f87fc9989f07a786e0e357482032a00c33176
RunChica <149354728+RunChica@users.noreply.github.com>
2026-07-27 23:44:59 +0800

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int inputNumber()
+{
+	int iNumber;
+	cin >> iNumber;
+	return iNumber;
+}
+
+int main()
+{
+	int firstValue;
+	cout << "Please key in first number: ";
+	firstValue = inputNumber();
+	cout << "Your number is " << firstValue << endl;
+
+	int secondValue;
+	cout << "Please key in first number: ";
+	secondValue = inputNumber();
+	cout << "Your number is " << secondValue << endl;
+
+	int thirdValue;
+	cout << "Please key in first number: ";
+	thirdValue = inputNumber();
+	cout << "Your number is " << thirdValue << endl;
+
+	int sum = firstValue + secondValue;
+	int diff = firstValue - thirdValue;
+	int product = firstValue * secondValue * thirdValue;
+
+	cout << "The sum of first and second number is " << "\t"; sum << "\n";
+	cout << "The diff og first and third number is " << "\t"; diff << "\n";
+	cout << "The product of all number is " << "\t"; product << "\n";
+
+	return 0;
+}
+
